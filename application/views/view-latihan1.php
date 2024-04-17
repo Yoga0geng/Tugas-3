ce3e8a7024c727e60e704fff80596ec26e02f923
YogaAgustirahayuM.F <ogenkepleu234@gmail.com>
2024-04-17 20:36:41 +0700

    first commit

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Hellowww Gaeesss Hayu Urang Sarerea Di Ajar web programming..!!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    Ieu hasil tina pamodelan rejeung methode penjumlahan nyaeta
+    <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+
+</html>
